44600390890c4f0241ba525dd95f9d4a8d007a53
jmkanmo <nebi25@naver.com>
2023-04-15 23:43:13 +0900

    first commit

---
+++ src/test/java/com/jmblog/freeblog_admin/FreeblogAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jmblog.freeblog_admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreeblogAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
