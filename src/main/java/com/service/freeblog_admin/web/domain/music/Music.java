5adae90ed084a868cf2a1abea205465859dd8038
jmkanmo <nebi25@naver.com>
2023-08-14 05:16:46 +0900

    music jpa 엔티티 작업 진행 + gzip compression 설정 활성화

--- src/main/java/com/service/freeblog_admin/web/domain/music/Music.java
+++ src/main/java/com/service/freeblog_admin/web/domain/music/Music.java
@@ -1,4 +1,32 @@
 package com.service.freeblog_admin.web.domain.music;
 
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "music")
 public class Music {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "music_id")
+    private Long id;
+
+    private String name;
+
+    private String artist;
+
+    private String url;
+
+    private String cover;
+
+    private String lrc;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "music_category_id")
+    private MusicCategory musicCategory;
 }
---
+++ src/main/java/com/service/freeblog_admin/web/domain/music/MusicCategory.java
@@ -0,0 +1,31 @@
+package com.service.freeblog_admin.web.domain.music;
+
+import com.service.freeblog_admin.web.domain.tag.Tag;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "music_category")
+public class MusicCategory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "music_category_id")
+    private Long id;
+
+    private String name;
+
+    @Fetch(FetchMode.SUBSELECT)
+    @OneToMany(mappedBy = "musicCategory")
+    private List<Music> musicList;
+}
